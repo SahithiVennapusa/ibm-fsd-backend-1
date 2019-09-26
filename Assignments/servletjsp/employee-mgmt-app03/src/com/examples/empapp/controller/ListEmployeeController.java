ee1c090b5a69350fdde158d9beab430cb296b4ab
Sahithi1109 <53862720+Sahithi1109@users.noreply.github.com>
2019-09-26 09:03:05 +0530

    app03

--- Assignments/servletjsp/employee-mgmt-app03/src/com/examples/empapp/controller/ListEmployeeController.java
+++ Assignments/servletjsp/employee-mgmt-app03/src/com/examples/empapp/controller/ListEmployeeController.java
@@ -1,7 +1,6 @@
 package com.examples.empapp.controller;
-
 import java.io.IOException;
-import java.util.List;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,25 +12,47 @@ import javax.servlet.http.HttpServletResponse;
 import com.examples.empapp.model.Employee;
 import com.examples.empapp.service.EmployeeService;
 
-@WebServlet("/listEmployees.do")
-public class ListEmployeeController extends HttpServlet {
 
-		EmployeeService empService = new EmployeeService();
+/**
+ * Servlet implementation class ListEmpolyeeController
+ */
+@WebServlet("/listEmployees.do")
+public class ListEmpolyeeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ListEmpolyeeController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 		
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			this.doPost(req, resp);
-		}
+		EmployeeService eSer=new EmployeeService();
 		
-		@Override
-		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-			List<Employee> employees = empService.getAll();
-			
-			System.out.println(employees);
-			
-			req.setAttribute("empList", employees);
-			
-			RequestDispatcher rd = req.getRequestDispatcher("listEmployees.jsp");
-			rd.forward(req, resp);
-		}
+		ArrayList<Employee> employees = eSer.viewAllEmployee();
+		
+		System.out.println(employees);
+		
+		request.setAttribute("empList", employees);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("ViewAllEmployees.jsp");
+		rd.forward(request, response);
+	}
+
 }
